a66696de7d9dd7029facdb0d1874adc24ec5a415
Shreshth Verma <shreshthverma31@gmail.com>
2026-08-27 23:48:47 +0530

    Added variables and calculation example

---
+++ 02_variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int length = 10;
+    int width = 5;
+    int area = length * width;
+
+    printf("Area of the Rectangle is: %d\n", area);
+    return 0;
+}
