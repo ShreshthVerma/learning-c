a9c72f5d64a57bafbf28b64aa5f29d1c3fc26008
Shreshth Verma <shreshthverma31@gmail.com>
2026-08-28 00:00:06 +0530

    Added data types example (int, float, char)

---
+++ 03_data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int studentID=101;
+  float score=82.25;
+  char grade='A';
+
+  printf("Student's ID:%d\n",studentID);
+  printf("Student's Score:%.2f\n",score);
+  printf("Student's Grade:%c\n",grade);
+
+  return 0;
+}
